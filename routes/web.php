8171ec4a9a95d2b9e9226faf04e897ee09d4d137
Bernardo Zaparoli <bernardozaparoli@hotmail.com>
2024-05-22 20:51:19 -0300

    ajustes nas rotas e nome do sistema

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\FeriasController;
 
 Route::get('/', function () {
-    return view('usuario.index');
+    return view('layout.app');
 });
+
+Route::resource('usuario', UsuarioController::class)->names([
+    'index' => 'usuario.lista',
+    'create' => 'usuario.adicionar',
+    'store' => 'usuario.salvar',
+    'show' => 'usuario.mostrar',
+    'edit' => 'usuario.editar',
+    'update' => 'usuario.atualizar',
+    'destroy' => 'usuario.excluir',
+]);
