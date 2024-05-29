95a895089280733f79177cac045531aaae77d436
romeudm <romeudm2001@hotmail.com>
2024-05-28 22:40:15 -0300

    Navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,33 +1,37 @@
 <!-- resources/views/partials/navbar.blade.php -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg bg-body-secondary rounded-5 mt-3 mb-4">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
-        <svg height="200" width="300" xmlns="http://www.w3.org/2000/svg" fill="white">
-        <image height="200" width="300" href="{{ asset('images/logocalendario.svg') }}" />
-        </svg>
-
-
-        
+        <img height="80" width="80" src="{{ asset('images/logocalendario.svg') }}" />
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Calendário</a>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarText">
+            <ul class="navbar-nav flex-fill justify-content-between">
+                <li class="nav-item p-2 mx-3">
+                    <a class="nav-link fs-4" href="#">Calendário</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Solicitações</a>
+                <li class="nav-item p-2 mx-3">
+                    <a class="nav-link fs-4" href="#">Solicitações</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Administrador</a>
-                </li>
+                <li class="nav-item dropdown p-2 mx-3">
+          <a class="nav-link dropdown-toggle fs-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Administrador
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Controle</a></li>
+            <li><a class="dropdown-item" href="#">op1</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
             </ul>
+            <div class="d-flex justify-content-right">
+                <span class="p-1 navbar-text my-auto px-4  fs-5">Nome</span>
+                <i class="p-1 fa-solid fa-circle-user" style="width: 80px; height: 80px;"></i>
+            </div>
         </div>
-        <div class="d-flex align-items-center">
-            <span class="navbar-text me-2">Nome</span>
-            <img src="{{ asset('images\icon-usr.svg') }}" alt="User Icon" style="width: 30px; height: 30px;" fill="black">
-        </div>
+
     </div>
 </nav>
