c15beb90cd99448241d86a5e9d2b41645106ecc8
romeudm <romeudm2001@hotmail.com>
2024-05-29 00:03:17 -0300

    Modificações na navbar e ajuste do esquema de cores

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <!-- resources/views/partials/navbar.blade.php -->
-<nav class="navbar navbar-expand-lg bg-body-secondary rounded-5 mt-3 mb-4">
+<nav class="navbar navbar-expand-lg bg-body-secondary rounded-5 my-4 mx-2 shadow" >
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
         <img height="80" width="80" src="{{ asset('images/logocalendario.svg') }}" />
@@ -8,15 +8,15 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-center" id="navbarText">
-            <ul class="navbar-nav flex-fill justify-content-between">
+            <ul class="navbar-nav flex-fill justify-content-between" >
                 <li class="nav-item p-2 mx-3">
-                    <a class="nav-link fs-4" href="#">Calendário</a>
+                    <a class="nav-link ps-5 fs-4 " href="#">Calendário</a>
                 </li>
                 <li class="nav-item p-2 mx-3">
                     <a class="nav-link fs-4" href="#">Solicitações</a>
                 </li>
                 <li class="nav-item dropdown p-2 mx-3">
-          <a class="nav-link dropdown-toggle fs-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle pe-5 fs-4" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Administrador
           </a>
           <ul class="dropdown-menu">
@@ -28,7 +28,7 @@
         </li>
             </ul>
             <div class="d-flex justify-content-right">
-                <span class="p-1 navbar-text my-auto px-4  fs-5">Nome</span>
+                <span class="p-1 navbar-text my-auto ms-5 ps-5  fs-5 text-end">Nome</span>
                 <i class="p-1 fa-solid fa-circle-user" style="width: 80px; height: 80px;"></i>
             </div>
         </div>
