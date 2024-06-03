57a04b4fb45cf72210b9872dbc9d22892de0b6d4
Bernardo Zaparoli <bernardozaparoli@hotmail.com>
2024-06-03 19:36:03 -0300

    Falto

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@ import '../css/app.css';
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 
-
 // Aqui que pode ser alterado as coisas dos select2
 $(function () {
     $('.select2').select2({
